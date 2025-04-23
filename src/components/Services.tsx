caeac22f8c4c20aa99bd8337cd2d146ee3b48ba1
Natnicha24 <natchaya110982@gmail.com>
2025-04-23 21:45:44 +0700

    change image path

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -18,7 +18,7 @@ function Service() {
      "
         >
           <img
-            src="./image/social-media.png"
+            src="/image/social-media.png"
             alt="social-media"
             className="w-full h-auto"
           />
@@ -42,7 +42,7 @@ function Service() {
      xl:col-span-1 xl:row-span-2"
         >
           <img
-            src="./image/font.png"
+            src="/image/font.png"
             alt="social-media"
             className=" w-[40%] h-auto"
           />
@@ -66,7 +66,7 @@ function Service() {
      xl:col-span-1 xl:row-span-2"
         >
           <img
-            src="./image/book.png"
+            src="/image/book.png"
             alt="book"
             className="mb-5 w-[35%] h-auto"
           />
@@ -89,7 +89,7 @@ function Service() {
      lg:col-span-2 lg:row-span-1 lg:col-start-2 lg:flex-row
      xl:col-span-2 xl:row-span-1 xl:col-start-2 xl:flex-row" 
         >
-          <img src="./image/sound.png" alt="sound" className="w-[35%] h-auto
+          <img src="/image/sound.png" alt="sound" className="w-[35%] h-auto
           sm:w-[35%] md:w-[20%] lg:w-[20%] xl:w-[20%] " />
           <div className="w-[70%] flex flex-col justify-center items-center gap-5
           sm:justify-center sm:items-center
@@ -115,92 +115,3 @@ function Service() {
 }
 
 export default Service;
-
-// <div
-// className="grid grid-cols-1 grid-rows-4 mt-5
-// sm:grid-cols-3 sm:grid-rows-2 gap-5 pt-[32px]"
-// >
-// <div
-//   className="col-span-1 row-span-1 row-start-1 bg-custom_pink rounded-xl
-// sm:col-span-1 sm:col-start-1 sm:row-span-2 py-10 px-10
-// flex flex-col justify-center items-center gap-5
-// "
-// >
-//   <img src="../../image/social-media.png" alt="" className="w-[100%]" />
-//   <div className="w-[100%] flex flex-col justify-start">
-// <span className="font-bold bg-custom_yellow px-5 py-2 rounded-3xl w-fit">
-//   Social
-// </span>
-//     <span className="text-left">
-//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-//       aut voluptates ipsum rerum quis! Iusto odit suscipit eveniet
-//       dolorum corporis cumque dolores, dolore reiciendis tempore
-//       similique sed illo incidunt cum!
-//     </span>
-//   </div>
-// </div>
-
-// <div
-//   className="col-start-1 row-start-2 col-span-1 row-span-1 bg-custom_pink rounded-xl
-//   sm:col-start-2 sm:col-span-1 sm:row-span-1 p-10
-
-// flex flex-col justify-center items-center gap-5
-// "
-// >
-//   <img src="../../image/font.png" alt="" className="w-[50%]" />
-//   <div className="w-[100%] flex flex-col justify-start">
-//     <span className="font-bold bg-custom_yellow px-5 py-2 rounded-3xl w-fit">
-//       Font
-//     </span>
-//     <span className="text-left">
-//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-//       aut voluptates ipsum rerum quis! Iusto odit suscipit eveniet
-//       dolorum corporis cumque dolores, dolore reiciendis tempore
-//       similique sed illo incidunt cum!
-//     </span>
-//   </div>
-// </div>
-// <div
-//   className="col-start-1 row-start-3 col-span-1 row-span-1
-// bg-custom_pink rounded-xl
-//  sm:col-start-3 sm:col-span-1 sm:row-span-1 p-10
-// flex flex-col justify-center items-center gap-5
-//  "
-// >
-//   <img src="../../image/book.png" alt="" className="w-[50%]" />
-//   <div className="w-[100%] flex flex-col justify-start">
-//     <span className="font-bold bg-custom_yellow px-5 py-2 rounded-3xl w-fit">
-//       Book
-//     </span>
-//     <span className="text-left">
-//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-//       aut voluptates ipsum rerum quis! Iusto odit suscipit eveniet
-//       dolorum corporis cumque dolores, dolore reiciendis tempore
-//       similique sed illo incidunt cum!
-//     </span>
-//   </div>
-// </div>
-// <div
-//   className="col-start-1 col-span-1 row-span-1 row-start-4
-// bg-custom_pink rounded-xl
-//  sm:col-start-2 sm:col-span-2 sm:row-span-1
-//  flex flex-col-reverse justify-center items-center gap-5
-//        sm:flex-row-reverse
-// "
-// >
-//   <div className="w-[50%] flex flex-col justify-start">
-//     <span className="font-bold bg-custom_yellow px-5 py-2 rounded-3xl w-fit">
-//       Sound
-//     </span>
-//     <span className="text-left">
-//       Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-//       aut voluptates ipsum rerum quis! Iusto odit suscipit eveniet
-//       dolorum corporis cumque dolores, dolore reiciendis tempore
-//       similique sed illo incidunt cum!
-//     </span>
-//   </div>
-//   <div className="w-[20%]">
-//     <img src="../../image/sound.png" alt="w-[50%]" />
-//   </div>
-// </div>
-// </div>
